19851bac4912421b4f3f1afd4c9c1855f7df46c8
katie <katie01bug05@gmail.com>
2020-01-28 23:36:42 -0700

    Homework Trigger

---
+++ project one/Assets/Scripts/HomeTriggerEvents.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+public class HomeTriggerEvents : MonoBehaviour
+{
+    public UnityEvent triggerEnterEvent;
+    private void OnTriggerEnter(Collider other)
+    {
+        triggerEnterEvent.Invoke();
+    }
+}
