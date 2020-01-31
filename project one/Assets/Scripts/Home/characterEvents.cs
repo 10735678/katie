3e5b1d46e5101f5c54dcb96a6c1883136260e7fb
katie <katie01bug05@gmail.com>
2020-01-30 21:56:08 -0700

    GameCharacterEventLearning

---
+++ project one/Assets/Scripts/Home/characterEvents.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class characterEvents : MonoBehaviour
+{
+    public UnityEvent downEvent, upEvent, enterEvent;
+
+    private void OnMouseDown()
+    {
+        downEvent.Invoke();
+    }
+    private void OnMouseUpAsButton()
+        {
+            upEvent.Invoke();
+        }
+    private void OnMouseDrag()
+    {
+        enterEvent.Invoke();
+    }
+    
+}
