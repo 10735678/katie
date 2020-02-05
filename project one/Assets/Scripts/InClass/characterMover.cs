46d375e1724da0d4d92445f359180201ae835824
katie <katie01bug05@gmail.com>
2020-02-05 16:33:00 -0700

    redoCharacterController

---
+++ project one/Assets/Scripts/InClass/characterMover.cs
@@ -0,0 +1,33 @@
+﻿
+using UnityEngine;
+
+public class characterMover : MonoBehaviour
+{
+    public CharacterController controller;
+    public Vector3 positionDirection;
+    public float gravity = -5f;
+    public float speed = 10f;
+    public float jumpforce = 30f;
+
+    void Start()
+    {
+
+ 
+    }
+    
+    
+    void Update()
+    {
+        positionDirection.x = Input.GetAxis("Vertical")* speed;
+        positionDirection.z = Input.GetAxis("Horizontal")* speed;
+        if (Input.GetButtonDown("Jump"))
+        {
+            positionDirection.y = jumpforce;
+        }
+
+        positionDirection.y -= gravity;
+
+        controller.Move(positionDirection * Time.deltaTime);
+    }
+}
+
