09af3872adcf20bbd5d4e5d98b616661cf715276
katie <katie01bug05@gmail.com>
2020-02-19 11:18:56 -0700

    Health bar image

---
+++ project one/Assets/Scripts/InClass/ImageBehaviour.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+
+[RequireComponent(typeof(Image))]
+public class ImageBehaviour : MonoBehaviour
+{
+    private Image imageObj;
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        imageObj = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
