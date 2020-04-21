201a20ab61e121201a6561d03665ab59bcea7e91
katie <katie01bug05@gmail.com>
2020-04-20 20:19:57 -0600

    MoreHealthandShooter

---
+++ New Unity Project (2)/Assets/Scripts/Datas/FloatData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class floatData : ScriptableObject
+{
+    public float value = 1f;
+
+    public void ChangeValue(float number)
+    {
+        value += number;
+    }
+}
+
